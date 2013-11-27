fdb1ca28cb12419e20871c73bef9538295598daf
Michael Küchler <Michael.Kuechler@local.ch>
2013-11-27 09:33:15 +0100

    default accumulators - sum

--- query/src/main/java/ch/julien/query/util/Accumulators.java
+++ query/src/main/java/ch/julien/query/util/Accumulators.java
@@ -9,7 +9,20 @@ public class Accumulators {
 			@SuppressWarnings("unchecked")
 			@Override public T accumulate(T accumulate, T source) {
 				Double sum = accumulate.doubleValue() + source.doubleValue();
-				return (T) sum;
+				
+				if (Double.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) sum;
+				} else if (Float.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Float) sum.floatValue();
+				} else if (Integer.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Integer) sum.intValue();
+				} else if (Long.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Long) sum.longValue();
+				} else if (Short.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Short) sum.shortValue();
+				} else {
+					throw new UnsupportedOperationException();
+				}
 			}
 		};
 	}
--- query/src/test/java/ch/julien/query/util/AccumulatorsTest.java
+++ query/src/test/java/ch/julien/query/util/AccumulatorsTest.java
@@ -8,10 +8,12 @@ public class AccumulatorsTest {
 
 	@Test
 	public void testSum() {
-		// test basic number types
-		assertThat(Accumulators.<Integer>sum().accumulate(1, 1)).isEqualTo(2);
+		// test supported number types
 		assertThat(Accumulators.<Double>sum().accumulate(1d, 1d)).isEqualTo(2d);
 		assertThat(Accumulators.<Float>sum().accumulate(1f, 1f)).isEqualTo(2f);
+		assertThat(Accumulators.<Integer>sum().accumulate(1, 1)).isEqualTo(2);
+		assertThat(Accumulators.<Long>sum().accumulate(1l, 1l)).isEqualTo(2l);
+		assertThat(Accumulators.<Short>sum().accumulate((short)1, (short)1)).isEqualTo((short)2);
 	}
 	
 }
