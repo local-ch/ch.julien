2df252f542d2fb97dff7c9e9159dac25e065741a
MiK <private@MiKs-MBP.local>
2013-11-28 01:03:59 +0100

    default accumulators - product

--- query/src/main/java/ch/julien/query/util/Accumulators.java
+++ query/src/main/java/ch/julien/query/util/Accumulators.java
@@ -2,13 +2,16 @@ package ch.julien.query.util;
 
 import ch.julien.common.delegate.Accumulator;
 
+
 public class Accumulators {
 
 	public static final <T extends Number> Accumulator<T, T> sum() {
 		return new Accumulator<T, T>() {
 			@SuppressWarnings("unchecked")
 			@Override public T accumulate(T accumulate, T source) {
-				if (Double.class.isAssignableFrom(accumulate.getClass())) {
+				if (Byte.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Byte) ((Integer) (accumulate.byteValue() + source.byteValue())).byteValue();
+				} else if (Double.class.isAssignableFrom(accumulate.getClass())) {
 					return (T) (Double) (accumulate.doubleValue() + source.doubleValue());
 				} else if (Float.class.isAssignableFrom(accumulate.getClass())) {
 					return (T) (Float) (accumulate.floatValue() + source.floatValue());
@@ -18,6 +21,29 @@ public class Accumulators {
 					return (T) (Long) (accumulate.longValue() + source.longValue());
 				} else if (Short.class.isAssignableFrom(accumulate.getClass())) {
 					return (T) (Short) ((Integer) (accumulate.shortValue() + source.shortValue())).shortValue();
+				} else {
+					throw new UnsupportedOperationException();
+				}
+			}
+		};
+	}
+	
+	public static final <T extends Number> Accumulator<T, T> product() {
+		return new Accumulator<T, T>() {
+			@SuppressWarnings("unchecked")
+			@Override public T accumulate(T accumulate, T source) {
+				if (Byte.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Byte) ((Integer) (accumulate.byteValue() * source.byteValue())).byteValue();
+				} else if (Double.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Double) (accumulate.doubleValue() * source.doubleValue());
+				} else if (Float.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Float) (accumulate.floatValue() * source.floatValue());
+				} else if (Integer.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Integer) (accumulate.intValue() * source.intValue());
+				} else if (Long.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Long) (accumulate.longValue() * source.longValue());
+				} else if (Short.class.isAssignableFrom(accumulate.getClass())) {
+					return (T) (Short) ((Integer) (accumulate.shortValue() * source.shortValue())).shortValue();
 				} else {
 					throw new UnsupportedOperationException();
 				}
--- query/src/test/java/ch/julien/query/util/AccumulatorsTest.java
+++ query/src/test/java/ch/julien/query/util/AccumulatorsTest.java
@@ -1,19 +1,88 @@
 package ch.julien.query.util;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.fail;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Test;
+
 
 public class AccumulatorsTest {
 
 	@Test
 	public void testSum() {
 		// test supported number types
-		assertThat(Accumulators.<Double>sum().accumulate(1d, 1d)).isEqualTo(2d);
-		assertThat(Accumulators.<Float>sum().accumulate(1f, 1f)).isEqualTo(2f);
-		assertThat(Accumulators.<Integer>sum().accumulate(1, 1)).isEqualTo(2);
-		assertThat(Accumulators.<Long>sum().accumulate(1l, 1l)).isEqualTo(2l);
-		assertThat(Accumulators.<Short>sum().accumulate((short)1, (short)1)).isEqualTo((short)2);
+		assertThat(Accumulators.<Byte>sum().accumulate((byte)2, (byte)3)).isEqualTo((byte)5);
+		assertThat(Accumulators.<Double>sum().accumulate(2d, 3d)).isEqualTo(5d);
+		assertThat(Accumulators.<Float>sum().accumulate(2f, 3f)).isEqualTo(5f);
+		assertThat(Accumulators.<Integer>sum().accumulate(2, 3)).isEqualTo(5);
+		assertThat(Accumulators.<Long>sum().accumulate(2l, 3l)).isEqualTo(5l);
+		assertThat(Accumulators.<Short>sum().accumulate((short)2, (short)3)).isEqualTo((short)5);
+	}
+	
+	@Test
+	public void testSum_unsupportedTypes() {
+		List<Number> unsupportedInputs = Arrays.asList(
+				new BigInteger("1"),
+				new BigDecimal(1),
+				new AtomicInteger(1),
+				new AtomicLong(1)
+				);
+		for (Number input : unsupportedInputs) {
+			try {
+				_testSum_unsupportedTypes(input);
+				fail("Expected exception with type " + input.getClass().getName() + ": " + UnsupportedOperationException.class.getName());
+			} catch (UnsupportedOperationException e) {
+				// test passed
+			} catch (RuntimeException e) {
+				fail("Unexpected exception with type " + input.getClass().getName() + ", expected<" + UnsupportedOperationException.class.getName() + "> but was<" + e.getClass().getName() + ">");
+			}
+		}
+	}
+	
+	private <T extends Number> void _testSum_unsupportedTypes(T input) {
+		Accumulators.<T>sum().accumulate(input, input);
+	}
+	
+	@Test
+	public void testProduct() {
+		// test supported number types
+		assertThat(Accumulators.<Byte>product().accumulate((byte)2, (byte)3)).isEqualTo((byte)6);
+		assertThat(Accumulators.<Double>product().accumulate(2d, 3d)).isEqualTo(6d);
+		assertThat(Accumulators.<Float>product().accumulate(2f, 3f)).isEqualTo(6f);
+		assertThat(Accumulators.<Integer>product().accumulate(2, 3)).isEqualTo(6);
+		assertThat(Accumulators.<Long>product().accumulate(2l, 3l)).isEqualTo(6l);
+		assertThat(Accumulators.<Short>product().accumulate((short)2, (short)3)).isEqualTo((short)6);
+	}
+	
+	@Test
+	public void testProduct_unsupportedTypes() {
+		List<Number> unsupportedInputs = Arrays.asList(
+				new BigInteger("1"),
+				new BigDecimal(1),
+				new AtomicInteger(1),
+				new AtomicLong(1)
+				);
+		for (Number input : unsupportedInputs) {
+			try {
+				_testProduct_unsupportedTypes(input);
+				fail("Expected exception with type " + input.getClass().getName() + ": " + UnsupportedOperationException.class.getName());
+			} catch (UnsupportedOperationException e) {
+				// test passed
+			} catch (RuntimeException e) {
+				fail("Unexpected exception with type " + input.getClass().getName() + ", expected<" + UnsupportedOperationException.class.getName() + "> but was<" + e.getClass().getName() + ">");
+			}
+		}
+	}
+	
+	private <T extends Number> void _testProduct_unsupportedTypes(T input) {
+		Accumulators.<T>product().accumulate(input, input);
 	}
 	
 }
